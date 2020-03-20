13e7baa995600a15882620516b523d98cefd88bc
Peter Stritzinger <peter.satzinger@abf.at>
2020-03-20 08:02:40 +0100

    initial add

---
+++ branch/mybranch/java/Empty-Java-Project-Example/src/main/java/com/example/Empty/Java/Project/Example/EmptyJavaProjectExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Empty.Java.Project.Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyJavaProjectExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptyJavaProjectExampleApplication.class, args);
+	}
+
+}
---
+++ branch/mybranch/java/Empty-Java-Project-Example/src/test/java/com/example/Empty/Java/Project/Example/EmptyJavaProjectExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Empty.Java.Project.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptyJavaProjectExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
